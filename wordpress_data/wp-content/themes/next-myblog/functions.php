f82a05e2080936a856446ff83b93771aeea0b580
MasakiNishi <n24mfs@gmail.com>
2024-07-02 18:54:32 +0000

    [add] new image size for theme

---
+++ wordpress_data/wp-content/themes/next-myblog/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+add_image_size('2x-thumbnail', 300, 300, false);
+add_image_size('2x-medium', 960, 960, false);
+add_image_size('2x-large', 1600, 1600, false);
+add_image_size('2x-ogp', 2400, 2400, false);
+add_image_size('ogp', 1200, 1200, false);
+
+
+
+
