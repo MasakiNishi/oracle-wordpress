6dc5dd53eaf060de36a26d145541c05629ebb667
MasakiNishi <n24mfs@gmail.com>
2024-07-02 18:56:32 +0000

    [add] post thumbnails for theme

--- wordpress_data/wp-content/themes/next-myblog/functions.php
+++ wordpress_data/wp-content/themes/next-myblog/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+// アイキャッチ画像の設定
+add_theme_support('post-thumbnails');
+
+// 画像サイズの追加（retina対応）
 add_image_size('2x-thumbnail', 300, 300, false);
 add_image_size('2x-medium', 960, 960, false);
 add_image_size('2x-large', 1600, 1600, false);
